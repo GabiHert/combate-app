60f5c6b4e5aa9a5a87a508f7c9e9e08f274a52bb
Gabriel Guinter Herter <91509385+GabiHert@users.noreply.github.com>
2023-03-14 09:07:43 -0300

    updateCache addded

--- CombateApp/src/api/core/adapter/config.ts
+++ CombateApp/src/api/core/adapter/config.ts
@@ -1,7 +1,7 @@
-import { IcreateIconProps } from 'native-base/lib/typescript/components/primitives/Icon/types';
-import { IConfigsProps } from '../../interface/config-props';
-import { PConfig } from '../port/config-port';
-import { PRepository } from '../port/repository-port';
+import { IcreateIconProps } from "native-base/lib/typescript/components/primitives/Icon/types";
+import { IConfigsProps } from "../../interface/config-props";
+import { PConfig } from "../port/config-port";
+import { PRepository } from "../port/repository-port";
 
 export class AConfig implements PConfig {
   private cache: IConfigsProps;
@@ -11,14 +11,14 @@ export class AConfig implements PConfig {
   async update(config: IConfigsProps) {
     this.cache = config;
     const str = JSON.stringify(config);
-    await this.repository.persist('CONFIG', str);
+    await this.repository.persist("CONFIG", str);
   }
   getCache(): IConfigsProps {
     return this.cache;
   }
   async updateCache(): Promise<void> {
-    const str = await this.repository.get('CONFIG');
-    // const config = ;
+    const str = await this.repository.get("CONFIG");
+    const config = JSON.parse(str);
     this.cache = config;
   }
 }
