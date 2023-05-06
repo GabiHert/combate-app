513d36867e443a04f4282cce29eeb044f41a6e65
GabiHert <hertergabriel4@gmail.com>
2023-05-06 16:17:44 -0300

    finished protocol-rules tdd [test][feat]

--- CombateApp/src/internal/core/rules/protocol-rules.ts
+++ CombateApp/src/internal/core/rules/protocol-rules.ts
@@ -1,5 +1,6 @@
 import { CheckSumBuilder, checkSumBuilder } from '../builder/check-sum-builder';
 import { DResponse } from '../dto/response-dto';
+import { Status } from '../enum/status';
 import { ValidationErrorType } from '../error/error-type';
 import { logger, PLogger } from '../port/logger-port';
 
@@ -9,37 +10,101 @@ export class ProtocolRules {
     private readonly _checkSumBuilder: CheckSumBuilder
   ) {}
   V4(protocol: string) {
-    if (protocol.length < 23) {
-      this._logger.warn({
+    try {
+      this._logger.info({
         event: 'ProtocolRules.V4',
-        details: 'Process warn',
-        warn: 'protocol length different from expected',
+        details: 'Process started',
+        protocol,
       });
-      throw new ValidationErrorType('Protocol length does not mach rules');
-    }
 
-    if (protocol[0] != '&') {
-    }
+      if (protocol.length < 23) {
+        this._logger.warn({
+          event: 'ProtocolRules.V4',
+          details: 'Process warn',
+          warn: 'protocol length different from expected',
+        });
+        throw new ValidationErrorType('Protocol length does not mach rules');
+      }
 
-    const cs = this._checkSumBuilder.build(protocol.substring(1, 10));
-    if (cs != protocol[10]) {
-      this._logger.warn({
+      if (protocol.split(',')[0].length != 11) {
+        this._logger.warn({
+          event: 'ProtocolRules.V4',
+          details: 'Process warn',
+          warn: 'protocol length different from expected',
+        });
+        throw new ValidationErrorType('Protocol length does not mach rules');
+      }
+
+      if (protocol[0] != '&') {
+        this._logger.warn({
+          event: 'ProtocolRules.V4',
+          details: 'Process warn',
+          warn: 'protocol header different from expected',
+        });
+        throw new ValidationErrorType('Protocol header does not mach rules');
+      }
+
+      const cs = this._checkSumBuilder.build(protocol.substring(1, 10));
+      if (cs != protocol[10]) {
+        this._logger.warn({
+          event: 'ProtocolRules.V4',
+          details: 'Process warn',
+          warn: 'protocol checkSum different from expected',
+        });
+        throw new ValidationErrorType('Protocol checkSum does not mach rules');
+      }
+
+      if (!/^\d+$/.test(protocol[1]) && !/^\d+$/.test(protocol[2])) {
+        this._logger.warn({
+          event: 'ProtocolRules.V4',
+          details: 'Process warn',
+          warn: 'protocol wheelBoltsCounter different from expected',
+        });
+        throw new ValidationErrorType('Protocol wheelBoltsCounter does not mach rules');
+      }
+
+      try {
+        new Status(protocol[3]);
+      } catch (err) {
+        this._logger.warn({
+          event: 'ProtocolRules.V4',
+          details: 'Process warn',
+          warn: 'protocol status different from expected',
+        });
+        throw new ValidationErrorType('Protocol status does not mach rules');
+      }
+
+      if (!/^\d+$/.test(protocol[3]) && !/^\d+$/.test(protocol[4]) && !/^\d+$/.test(protocol[5])) {
+        this._logger.warn({
+          event: 'ProtocolRules.V4',
+          details: 'Process warn',
+          warn: 'protocol wheelBoltsCounter different from expected',
+        });
+        throw new ValidationErrorType('Protocol errorCode does not mach rules');
+      }
+
+      const protocolEnd = protocol.substring(protocol.length - 2, protocol.length);
+      if (protocolEnd != '\r\n') {
+        this._logger.warn({
+          event: 'ProtocolRules.V4',
+          details: 'Process warn',
+          warn: 'protocolEnd different from expected',
+          protocolEnd,
+        });
+        throw new ValidationErrorType('Protocol end different from expected');
+      }
+
+      this._logger.info({
         event: 'ProtocolRules.V4',
-        details: 'Process warn',
-        warn: 'protocol checkSum different from expected',
+        details: 'Process finished',
       });
-      throw new ValidationErrorType('Protocol checkSum not mach rules');
-    }
-
-    const protocolEnd = protocol.substring(protocol.length - 2, protocol.length);
-    if (protocolEnd != '\r\n') {
-      this._logger.warn({
+    } catch (err) {
+      this._logger.error({
         event: 'ProtocolRules.V4',
-        details: 'Process warn',
-        warn: 'protocolEnd different from expected',
-        protocolEnd,
+        details: 'Process error',
+        error: err.message,
       });
-      throw new ValidationErrorType('Protocol end different from expected');
+      throw err;
     }
   }
 }
--- CombateApp/test/internal/core/rules/protocol-rules.test.ts
+++ CombateApp/test/internal/core/rules/protocol-rules.test.ts
@@ -3,6 +3,7 @@ import {
   CheckSumBuilder,
   checkSumBuilder,
 } from '../../../../src/internal/core/builder/check-sum-builder';
+import { ValidationErrorType } from '../../../../src/internal/core/error/error-type';
 import { ProtocolRules } from '../../../../src/internal/core/rules/protocol-rules';
 import { LoggerMock } from '../../../mock/logger-mock';
 
@@ -14,24 +15,89 @@ describe('protocol-riles test', () => {
     loggerMocked.clear();
   });
 
+  it('should not throw an error when request is valid', () => {
+    const protocol =
+      '&00S000xxx' +
+      checkSumBuilder.build('00S000xxx') +
+      ',001220.00,A,3001.89425,S,05109.81024,W,0.374,,240719,,,A75\r\n';
+    protocolRules.V4(protocol);
+    expect(loggerMocked.infoCalled).toBeGreaterThanOrEqual(1);
+    expect(loggerMocked.errorCalled).toBe(0);
+    expect(loggerMocked.warnCalled).toBe(0);
+  });
+
   it('should throw an error when V4 protocol length is invalid', () => {
     const protocol = '&';
-    expect(()=>protocolRules.V4(protocol)).toThrow(new );
+    expect(() => protocolRules.V4(protocol)).toThrow(
+      new ValidationErrorType('Protocol length does not mach rules')
+    );
+    expect(loggerMocked.infoCalled).toBeGreaterThanOrEqual(1);
+    expect(loggerMocked.errorCalled).toBeGreaterThanOrEqual(1);
+    expect(loggerMocked.warnCalled).toBeGreaterThanOrEqual(1);
+  });
+  it('should throw an error when V4 protocol has no full information', () => {
+    const protocol = '&00S000xxU,001220.00,A,3001.89425,S,05109.81024,W,0.374,,240719,,,A75\r\n';
+    expect(() => protocolRules.V4(protocol)).toThrow(
+      new ValidationErrorType('Protocol length does not mach rules')
+    );
     expect(loggerMocked.infoCalled).toBeGreaterThanOrEqual(1);
     expect(loggerMocked.errorCalled).toBeGreaterThanOrEqual(1);
     expect(loggerMocked.warnCalled).toBeGreaterThanOrEqual(1);
   });
   it('should throw an error when V4 protocol header is invalid', () => {
     const protocol = '!00S000xxxU,001220.00,A,3001.89425,S,05109.81024,W,0.374,,240719,,,A75\r\n';
-    protocolRules.V4(protocol);
+    expect(() => protocolRules.V4(protocol)).toThrow(
+      new ValidationErrorType('Protocol header does not mach rules')
+    );
     expect(loggerMocked.infoCalled).toBeGreaterThanOrEqual(1);
     expect(loggerMocked.errorCalled).toBeGreaterThanOrEqual(1);
     expect(loggerMocked.warnCalled).toBeGreaterThanOrEqual(1);
   });
-  it('should throw an error when V4 protocol checkSum is invalid', () => {});
-  it('should throw an error when V4 protocol wheelBoltsCount is invalid', () => {});
-  it('should throw an error when V4 protocol status is invalid', () => {});
-  it('should throw an error when V4 protocol errorCode is invalid', () => {});
+  it('should throw an error when V4 protocol checkSum is invalid', () => {
+    const protocol = '&00S000xxxS,001220.00,A,3001.89425,S,05109.81024,W,0.374,,240719,,,A75\r\n';
+    expect(() => protocolRules.V4(protocol)).toThrow(
+      new ValidationErrorType('Protocol checkSum does not mach rules')
+    );
+    expect(loggerMocked.infoCalled).toBeGreaterThanOrEqual(1);
+    expect(loggerMocked.errorCalled).toBeGreaterThanOrEqual(1);
+    expect(loggerMocked.warnCalled).toBeGreaterThanOrEqual(1);
+  });
+  it('should throw an error when V4 protocol wheelBoltsCount are invalid', () => {
+    const protocol =
+      '&xxS000xxx' +
+      checkSumBuilder.build('xxS000xxx') +
+      ',001220.00,A,3001.89425,S,05109.81024,W,0.374,,240719,,,A75\r\n';
+    expect(() => protocolRules.V4(protocol)).toThrow(
+      new ValidationErrorType('Protocol wheelBoltsCounter does not mach rules')
+    );
+    expect(loggerMocked.infoCalled).toBeGreaterThanOrEqual(1);
+    expect(loggerMocked.errorCalled).toBeGreaterThanOrEqual(1);
+    expect(loggerMocked.warnCalled).toBeGreaterThanOrEqual(1);
+  });
+  it('should throw an error when V4 protocol status is invalid', () => {
+    const protocol =
+      '&00R000xxx' +
+      checkSumBuilder.build('00R000xxx') +
+      ',001220.00,A,3001.89425,S,05109.81024,W,0.374,,240719,,,A75\r\n';
+    expect(() => protocolRules.V4(protocol)).toThrow(
+      new ValidationErrorType('Protocol status does not mach rules')
+    );
+    expect(loggerMocked.infoCalled).toBeGreaterThanOrEqual(1);
+    expect(loggerMocked.errorCalled).toBeGreaterThanOrEqual(1);
+    expect(loggerMocked.warnCalled).toBeGreaterThanOrEqual(1);
+  });
+  it('should throw an error when V4 protocol errorCode is invalid', () => {
+    const protocol =
+      '&00EWWWxxx' +
+      checkSumBuilder.build('00EWWWxxx') +
+      ',001220.00,A,3001.89425,S,05109.81024,W,0.374,,240719,,,A75\r\n';
+    expect(() => protocolRules.V4(protocol)).toThrow(
+      new ValidationErrorType('Protocol errorCode does not mach rules')
+    );
+    expect(loggerMocked.infoCalled).toBeGreaterThanOrEqual(1);
+    expect(loggerMocked.errorCalled).toBeGreaterThanOrEqual(1);
+    expect(loggerMocked.warnCalled).toBeGreaterThanOrEqual(1);
+  });
 });
 
 export {};
