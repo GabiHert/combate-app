8050ced76d689ece6084a18e3fd7da85196eaf8e
GabiHert <hertergabriel4@gmail.com>
2023-05-12 20:32:37 -0300

    finished instances [feat]

--- CombateApp/src/app/instance/instance.ts
+++ CombateApp/src/app/instance/instance.ts
@@ -3,18 +3,20 @@ import { CombateApp } from '../../cmd/combateapp/combate-app';
 import { Validator } from '../../cmd/formvalidator/form-validator';
 import { ABluetooth } from '../../internal/adapter/bluetooth/bluetooth';
 import { AConfigCache } from '../../internal/adapter/cache/config-cache';
-import { DEFAULT_CONFIG } from '../../internal/config/config';
+import { APreExecutionConfigCache } from '../../internal/adapter/cache/pre-execution-config-cache';
+import { AAsyncStorage } from '../../internal/adapter/repository/async-storage-adapter';
+import { DEFAULT_CONFIG, DEFAULT_PRE_EXECUTION_CONFIG } from '../../internal/config/config';
 import { CbServiceFactory } from '../../internal/core/factory/cb-service-factory';
 import { requestFactory } from '../../internal/core/factory/request-factory';
 import { responseDtoParser } from '../../internal/core/parser/response-dto-parser';
 import { logger } from '../../internal/core/port/logger-port';
-import { repository } from '../../internal/core/port/repository-port';
 import { protocolRules } from '../../internal/core/rules/protocol-rules';
 import { CsvTableService } from '../../internal/core/service/csv-table-service';
 const bluetooth = new ABluetooth(logger);
 const csvTableService = new CsvTableService(logger, undefined);
 const cbServiceFactory = new CbServiceFactory(logger, bluetooth, responseDtoParser);
 
+const repository = new AAsyncStorage(logger);
 export const combateApp = new CombateApp(
   logger,
   cbServiceFactory,
@@ -22,7 +24,11 @@ export const combateApp = new CombateApp(
   requestFactory,
   protocolRules
 );
-
-export const config = new AConfigCache(repository, DEFAULT_CONFIG);
+export const configCache = new AConfigCache(logger, repository, DEFAULT_CONFIG);
 export const bluetoothApp = new BluetoothApp(logger, bluetooth);
-export const validator = new Validator(logger, config);
+export const validator = new Validator(logger, configCache);
+export const preExecutionConfigCache = new APreExecutionConfigCache(
+  logger,
+  repository,
+  DEFAULT_PRE_EXECUTION_CONFIG
+);
