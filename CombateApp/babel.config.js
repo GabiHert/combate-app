59f9e64f670ade8a0cafcc592cd7b0d3b3f1d6a2
GabiHert <hertergabriel4@gmail.com>
2023-05-17 20:36:56 -0300

    removed preset-env

--- CombateApp/babel.config.js
+++ CombateApp/babel.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    '@babel/preset-env',
-    '@babel/preset-typescript',
-  ],
+  presets: ['module:metro-react-native-babel-preset', '@babel/preset-typescript'],
   plugins: ['react-native-reanimated/plugin'],
 };
